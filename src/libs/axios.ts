b80015f14f8b266cf88bd50d9003f6c89102787b
Nama Kamu <nizarrarfan@gmail.com>
2025-08-13 22:51:21 +0700

    fix: streamline token handling in axios instance and improve error management

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -9,15 +9,11 @@ export const axiosInstance = axios.create({
 // /handle request axios
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      throw new axios.Cancel("Unauthorized: No token");
-    }
-    console.log("tokenbeckend", token);
-    //kalo ada token
-    config.headers.Authorization = `Bearer ${token}`;
-
-    return config;
+   const token = localStorage.getItem("token");
+   if (token) {
+     config.headers.Authorization = `Bearer ${token}`;
+   }
+   return config;
   },
   (error) => {
     return Promise.reject(error);
@@ -36,10 +32,11 @@ axiosInstance.interceptors.response.use(
     if (error.response.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
-        const res = await axiosInstance.post("/auth/refreshToken", null, {
+        const res = await axios.post("/auth/refreshToken", null, {
           withCredentials: true,
         });
-        const newAccessToken = res.data.accesToken;
+        console.log("res", res);
+        const newAccessToken = res.data.accessToken;
         localStorage.setItem("token", newAccessToken);
         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
         return axiosInstance(originalRequest);
