8970b0172b1a57a4f35e4f179f3ebb40626c131f
Nama Kamu <nizarrarfan@gmail.com>
2025-08-22 15:28:52 +0700

    add types midtrans

---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type { ProductTypes } from "./products";
---
+++ src/types/products.ts
@@ -0,0 +1,14 @@
+export interface ProductTypes {
+  count: number;
+  createdAt: Date;
+  description: string;
+  editededAt: Date;
+  email: string;
+  id: number;
+  images: any;
+  name: string;
+  price: number;
+  quantity: number;
+  rate: number;
+  stock: number;
+}
